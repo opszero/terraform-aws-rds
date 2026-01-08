aabf059d5666b3d0e76549ab7d50414dfe142bf9
Sohan Yadav <sohanyadavdevops@gmail.com>
2026-01-08 17:09:54 +0530

    fix terraform plan

--- main.tf
+++ main.tf
@@ -196,7 +196,6 @@ resource "aws_db_instance" "this" {
 
   db_name                             = var.db_name
   username                            = var.db_username
-  manage_master_user_password         = var.manage_master_user_password
   password                            = var.password
   port                                = var.port
   domain                              = var.domain
