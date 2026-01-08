55b4d4e4f48d28f7d9b975bb2d9c985a114cd76d
Sohan Yadav <sohanyadavdevops@gmail.com>
2026-01-08 18:29:31 +0530

    fix terraform plan

--- variables.tf
+++ variables.tf
@@ -580,3 +580,9 @@ variable "option_group_name" {
   description = "The ID of the VPC that the instance security group belongs to."
   sensitive   = true
 }
+
+variable "final_snapshot_identifier" {
+  type        = string
+  default     = "metabase-prod-postgres"
+  description = "The ID of the VPC that the instance security group belongs to."
+}
