b60bb634d42c6c2a8b802f66ad954c0b7466678f
adhruvateja <adhruvateja00@gmail.com>
2026-07-09 21:12:31 +0530

    Encryption on by default

--- example/replica-mysql/main.tf
+++ example/replica-mysql/main.tf
@@ -34,6 +34,7 @@ module "mysql" {
   snapshot_identifier    = ""
   kms_key_id             = ""
   enabled_read_replica   = true
+  storage_encrypted = true
 
   db_name     = "replica"
   db_username = "replica_mysql"
